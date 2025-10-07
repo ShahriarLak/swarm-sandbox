8948fe4333022e633081ff73651281b9c31ecc61
ShahriarLak <shahriar.lak1@gmail.com>
2025-10-07 22:10:07 +0100

    Update postcss.config.mjs (#5)

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.mjs (ESM)
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
-
-export default config;
